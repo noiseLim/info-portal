8fc6d457bec4082a6762e764d4cce3dddc7ff72d
noiseLim <noiselim@gmail.com>
2023-02-05 19:08:22 +0530

    improved routing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,8 @@
-import { Link, Route, Routes } from 'react-router-dom';
-import { Suspense } from 'react';
+import { Link } from 'react-router-dom';
 
-import { AboutPage } from 'pages/AboutPage';
-import { MainPage } from 'pages/MainPage';
 import { useTheme } from './providers/ThemeProvider';
 import { classNames } from 'shared/lib/classNames/classNames';
+import { AppRouter } from './providers/router';
 
 import './styles/index.scss';
 
@@ -16,12 +14,7 @@ const App = () => {
       <button onClick={toggleTheme}>CHANGE THEME</button>
       <Link to={'/'}>ГЛАВНАЯ</Link>
       <Link to={'/about'}>О САЙТЕ</Link>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path={'/about'} element={<AboutPage />} />
-          <Route path={'/'} element={<MainPage />} />
-        </Routes>
-      </Suspense>
+      <AppRouter />
     </div>
   );
 };
