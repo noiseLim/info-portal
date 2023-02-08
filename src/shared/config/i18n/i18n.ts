5d18de2b5c7c19922d4c6888327e8484013b6739
noiseLim <noiselim@gmail.com>
2023-02-08 15:20:01 +0530

    added minor improvements with i18next

--- src/shared/config/i18n/i18n.ts
+++ src/shared/config/i18n/i18n.ts
@@ -15,9 +15,9 @@ i18n
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
-    // backend: {
-    //   loadPath: '/locales/{{lng}}/{{ns}}.json',
-    // },
+    backend: {
+      loadPath: '/locales/{{lng}}/{{ns}}.json',
+    },
   });
 
 export default i18n;
