c4573c7963cf8fd4c346979de430559e30bf83f1
noiseLim <noiselim@gmail.com>
2023-02-12 20:19:48 +0530

    disabled spam in the console from i18next

--- src/shared/config/i18n/i18n.ts
+++ src/shared/config/i18n/i18n.ts
@@ -10,7 +10,7 @@ i18n
   .use(initReactI18next)
   .init({
     fallbackLng: 'ru',
-    debug: __IS_DEV__ ? true : false,
+    debug: false, // __IS_DEV__ ? true : false,
 
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
