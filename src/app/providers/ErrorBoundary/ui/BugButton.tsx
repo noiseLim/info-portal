8f00f87f17780175d3952df1f17faa9812dfd240
noiseLim <noiselim@gmail.com>
2023-02-12 18:49:20 +0530

    added ErrorBoundary

---
+++ src/app/providers/ErrorBoundary/ui/BugButton.tsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { Button } from 'shared/ui/Button/Button';
+
+// Component for testing ErrorBoundary
+export const BugButton = () => {
+  const [error, setError] = useState(false);
+  const { t } = useTranslation();
+
+  useEffect(() => {
+    if (error) {
+      throw new Error();
+    }
+  }, [error]);
+
+  const onThrow = () => setError(true);
+
+  return <Button onClick={onThrow}>{t('Throw Error')}</Button>;
+};
---
+++ src/widgets/PageError/ui/PageError.tsx
@@ -0,0 +1,23 @@
+import { useTranslation } from 'react-i18next';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Button } from 'shared/ui/Button/Button';
+import style from './pageError.module.scss';
+
+interface PageErrorProps {
+  className?: string;
+}
+export const PageError = ({ className }: PageErrorProps) => {
+  const { t } = useTranslation();
+
+  const reloadPage = () => {
+    // eslint-disable-next-line no-restricted-globals
+    location.reload();
+  };
+
+  return (
+    <div className={classNames(style.pageError, {}, [className])}>
+      <p>{t('An unexpected error has occurred')}</p>
+      <Button onClick={reloadPage}>{t('Reload page')}</Button>
+    </div>
+  );
+};
