6dd3612b77f3060cfcf9404f7fda0036ecd3ffbf
noiseLim <noiselim@gmail.com>
2023-05-05 22:22:59 +0300

    added test for isolated  components  with cypress

--- src/shared/lib/tests/componentRender/componentRender.tsx
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -6,23 +6,48 @@ import { ReducersMapObject } from '@reduxjs/toolkit';
 
 import i18nForTest from '@/shared/config/i18n/i18nForTest';
 import { StateSchema, StoreProvider } from '@/app/providers/StoreProvider';
+import { Theme } from '@/shared/const/theme';
+// eslint-disable-next-line noiselim-plugin/layer-imports
+import { ThemeProvider } from '@/app/providers/ThemeProvider';
+// eslint-disable-next-line noiselim-plugin/layer-imports
+import '@/app/styles/index.scss';
 
 export interface componentRenderProps {
   route?: string;
   initialState?: DeepPartial<StateSchema>;
   asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>;
+  theme?: Theme;
+}
+
+interface TestProviderProps {
+  children: ReactNode;
+  options?: componentRenderProps;
+}
+
+export function TestProvider(props: TestProviderProps) {
+  const { children, options = {} } = props;
+  const {
+    route = '/',
+    initialState,
+    asyncReducers,
+    theme = Theme.LIGHT,
+  } = options;
+  return (
+    <MemoryRouter initialEntries={[route]}>
+      <StoreProvider initialState={initialState} asyncReducers={asyncReducers}>
+        <I18nextProvider i18n={i18nForTest}>
+          <ThemeProvider>
+            <div className={`app ${theme}`}>{children}</div>
+          </ThemeProvider>
+        </I18nextProvider>
+      </StoreProvider>
+    </MemoryRouter>
+  );
 }
 
 export function componentRender(
   component: ReactNode,
   options: componentRenderProps = {}
 ) {
-  const { route = '/', initialState, asyncReducers } = options;
-  return render(
-    <MemoryRouter initialEntries={[route]}>
-      <StoreProvider initialState={initialState} asyncReducers={asyncReducers}>
-        <I18nextProvider i18n={i18nForTest}>{component}</I18nextProvider>
-      </StoreProvider>
-    </MemoryRouter>
-  );
+  return render(<TestProvider options={options}>{component}</TestProvider>);
 }
